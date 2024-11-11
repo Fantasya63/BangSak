f902a3b45fc1347d811f0ef4761197baa6d710c1
Jansen Baloaloa <baloaloajansen.school@gmail.com>
2024-11-11 20:48:46 +0800

    checkpoint before nuke

---
+++ scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_create_btn_pressed():
+	pass # Replace with function body.
+
+
+func _on_join_btn_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_btn_pressed():
+	get_tree().quit()
