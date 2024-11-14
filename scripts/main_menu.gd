6d68604d58ddc79f145a7e3922684303a25361f4
Jansen Baloaloa <baloaloajansen.school@gmail.com>
2024-11-14 16:23:36 +0800

    checkpoint

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,16 @@
 extends Node
 
+@export_node_path("Popup")
+var join_popup_path
 
+@onready var join_popup = get_node(join_popup_path)
 
 func _on_create_btn_pressed():
 	pass # Replace with function body.
 
 
 func _on_join_btn_pressed():
+	join_popup.visible = true
 	pass # Replace with function body.
 
 
